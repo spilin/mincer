7a8df3c2e2b15f6267cfd9927dcffba0447570df
Alex Krasynskyi <lyoshakr@gmail.com>
2016-07-06 19:56:33 +0300

    removed ar version dependency

--- lib/mincer/version.rb
+++ lib/mincer/version.rb
@@ -1,7 +1,7 @@
 module Mincer
 
   def self.version
-    Gem::Version.new '0.2.6'
+    Gem::Version.new '0.2.7'
   end
 
   module VERSION #:nodoc:
@@ -9,10 +9,3 @@ module Mincer
     STRING = Mincer.version.to_s
   end
 end
-
-
-
-
-
-
-
